8af6bcbb54460d123ccdc3b2a1fac9510a9ead59
Nitrax <Martin.Engen@outlook.com>
2017-01-04 12:41:22 +0100

    MongoDB connected. Misc configuration on server.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-/**
- * Created by Mello on 1/4/2017.
- */
+
+// ============ setup ============
 
 // Express init. framework
 var express = require('express');
@@ -8,6 +7,39 @@ var express = require('express');
 // Express setup
 var app = express();
 
+var mongoose = require ('mongoose'); // mongoose database
+var morgan = require('morgan'); //log requests to the console
+var bodyParser = require('body-parser'); //Get information from HTML POST with express
+var methodOverride = require('method-override'); // DELETE and PUT with express
+
+
+
+// Database connection string.
+var uristring =
+    process.env.MONGODB_URI ||
+    'mongodb://db_user:123qwe@ds151228.mlab.com:51228/heroku_gxxcx978';
+
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+
+
+// ============ Configuration ============
+
+// Static files for this project.
+app.use(express.static(__dirname + '/public'));
+
+// Log all requests to the console window.
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({'extended': 'true'})); //parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); //Parse application/vnd.api+json as json
+app.use(methodOverride());
 // set port for this app. port works as a variable.
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,7 +48,7 @@ app.use(express.static(__dirname + '/public'));
 var cool = require('cool-ascii-faces');
 app.get('/', function (request, response) {
     response.send(cool())
-})
+});
 
 
 
